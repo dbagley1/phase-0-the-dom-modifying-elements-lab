397c45061d5b17d6d76b32999af809d889a4d39c
Darrian Bagley <dbagle14@gmail.com>
2022-03-19 22:38:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("#main").remove();
+var newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Darrian is the champion!";
+document.body.appendChild(newHeader);
